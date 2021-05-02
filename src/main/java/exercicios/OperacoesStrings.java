32bf69914a974ec265d329a43f9da27e8be0d667
Lucas Scaramelo <lucas.scaramelo@gmail.com>
2021-05-02 19:23:43 -0300

    feat: Operacoes com Strings

--- src/main/java/exercicios/OperacoesStrings.java
+++ src/main/java/exercicios/OperacoesStrings.java
@@ -20,6 +20,43 @@ public class OperacoesStrings {
     }
 
     public void cortarString() {
+        String s04 = original.substring(2);
+        String s05 = original.substring(2, 9);
+        System.out.println(s04 + s05);
+    }
 
+    public void trocandoOValorString() {
+        String s06 = original.replace('a', 'x'); // troca todos os caracteres "a" por "x"
+        String s07 = original.replace("abc", "xyz"); // troca "abc" por "xyz"
+        System.out.println(s06 + s07);
+    }
+
+    public void verificarOcorrenciaDaLetra() {
+        int i = original.indexOf("bc");
+        System.out.println("A ocorrência é na posição: " + i);
+    }
+
+    public void verificarUltimaOcorrencia() {
+        int j = original.lastIndexOf("bc");
+        System.out.println("Ultima ocorrência: " + j);
+    }
+
+    public void recortarString() {
+        String s = "potato apple lemon";
+
+        String[] vect = s.split(" "); // vetor de strings, o " " serve como separador
+        String word1 = vect[0];
+        String word2 = vect[1];
+        String word3 = vect[2];
+
+        // sem variavel
+        System.out.println(vect[0]);
+        System.out.println(vect[1]);
+        System.out.println(vect[2]);
+
+        // com variavel
+        System.out.println(word1);
+        System.out.println(word2);
+        System.out.println(word3);
     }
 }
