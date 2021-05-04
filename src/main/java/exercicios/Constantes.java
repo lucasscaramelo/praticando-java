6440f7a87735676a1e3d7fd1e4a881cbdcac12ca
Lucas Scaramelo <lucas.scaramelo@gmail.com>
2021-05-03 23:19:18 -0300

    feat: Constantes

---
+++ src/main/java/exercicios/Constantes.java
@@ -0,0 +1,5 @@
+package exercicios;
+
+public class Constantes {
+    public static final double COTACAO_DOLLAR = 5.30;  // static pode ser acessado sem criar objeto, final constante não muda o valor, sempre sera esse.
+}
