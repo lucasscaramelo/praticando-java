6b92ebaa0e96a98d7b9568d9e8aafd10010d2109
Lucas Scaramelo <lucas.scaramelo@gmail.com>
2021-05-11 21:54:15 -0300

    feat: Enumeradores

---
+++ src/main/java/exercicios/Enumeracao.java
@@ -0,0 +1,19 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class Enumeracao {
+
+    public void usandoEnumerador()  {
+        Enumeradores status;
+        status = Enumeradores.PROCESSING;
+
+        System.out.println(status);
+    }
+
+    public void converterStringParaEnum() {
+        Enumeradores status = Enumeradores.valueOf("Testando");
+        System.out.println(status);
+    }
+
+}
---
+++ src/main/java/exercicios/Enumeradores.java
@@ -0,0 +1,9 @@
+package exercicios;
+
+public enum Enumeradores {
+
+    PENDING_PAYMENT,
+    PROCESSING,
+    SHIPPED,
+    DELIVERED;
+}
